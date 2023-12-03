82c600569e290941a24682c4cad595e21bd366e9
Pranav Guruprasad Rao <pranavrao2500@gmail.com>
2023-12-03 21:34:31 +0530

    Quantum Walk algorithm modified

--- Quantum Solutions/QuantumWalk.py
+++ Quantum Solutions/QuantumWalk.py
@@ -3,46 +3,57 @@ from qiskit.circuit.library import XGate
 import numpy as np
 import matplotlib.pyplot as plt
 
-def init(qc): # Initialise the Walk Circuit
-    qc.x(qubits[0]) # Position 1
-    qc.h(coins[0]) # Coin vector put in superposition
 
-def add(coin,qubit): # Increment the position value
-    qc.cx(coins[0],qubits[0])
-    for i in range(1,len(qubits)):
-        for j in range(i):
-            qc.x(qubits[j])
-        qc.append(XGate().control(i+1),[coins[0]]+[qubits[j] for j in range(i+1)])
-        for j in range(i):
-            qc.x(qubits[j])
+def init():  # Initialise the Walk Circuit
+    qc = QuantumCircuit(coins, qubits, clas)
+    qc.x(qubits[0])  # Position 1
+    qc.h(coins[0])  # Coin vector put in superposition
+    return qc
 
-def sub(coin,qubit): # Decrement the position value
-    for i in range(1,len(qubits)):
-        for j in range(i):
-            qc.x(qubits[j])
-        qc.append(XGate().control(i+1),[coins[0]]+[qubits[j] for j in range(i+1)])
-        for j in range(i):
-            qc.x(qubits[j])
-        qc.cx(coins[0],qubits[0])
 
-def evol(coin,qubit): # Evolution of the walker vector with coin vector
-    qc = QuantumCircuit(coin,qubit)
+def add(coin, qubit):  # Increment the position value
+    qc = QuantumCircuit(coin, qubit)
+    qc.cx(coin[0], qubit[0])
+    for i in range(1, len(qubit)):
+        for j in range(i):
+            qc.x(qubit[j])
+        qc.append(XGate().control(i+1), [coin[0]]+[qubit[j] for j in range(i+1)])
+        for j in range(i):
+            qc.x(qubit[j])
+    return qc.to_gate()
+
+
+def sub(coin, qubit):  # Decrement the position value
+    qc = QuantumCircuit(coin, qubit)
+    for i in range(1, len(qubit)):
+        for j in range(i):
+            qc.x(qubit[j])
+        qc.append(XGate().control(i+1), [coin[0]]+[qubit[j] for j in range(i+1)])
+        for j in range(i):
+            qc.x(qubit[j])
+        qc.cx(coin[0], qubit[0])
+    return qc.to_gate()
+
+
+def evol(coin, qubit):  # Evolution of the walker vector with coin vector
+    qc = QuantumCircuit(coin, qubit)
     qc.h(coin)
-    add(coin,qubit) # add
+    qc.append(add(coin, qubit), list(range(qc.num_qubits))) # add
     qc.x(coin)
-    sub(coin,qubit) # sub
+    qc.append(sub(coin, qubit), list(range(qc.num_qubits))) # add # sub
     qc.x(coin)
+    return qc.to_gate()
 
 
-def walk(qc,N): # Walk N times
-    lr = np.random.bit_generator
+def walk(coin, qubit, N):  # Walk N times
     for i in range(N):
-        evol(qc,np.random.randint(2))
+        qc.append(evol(coin, qubit), list(range(qc.num_qubits)))
 
-qubits = QuantumRegister(7,'nodes')
-coins = QuantumRegister(1,'coins')
-clas = ClassicalRegister(7,'meas')
-qc = QuantumCircuit(coins,qubits,clas)
-init(qc)
-walk(qc,len(qubits))
-qc.measure(qubits,clas)
+
+qubits = QuantumRegister(7, 'nodes')
+coins = QuantumRegister(1, 'coins')
+clas = ClassicalRegister(7, 'meas')
+
+qc = init()
+walk(coins, qubits, len(qubits))
+qc.measure(qubits, clas)
