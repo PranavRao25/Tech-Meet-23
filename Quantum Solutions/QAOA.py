78fa0d9db811cb798ea54bc5fc6a12ed6a48c213
Pranav Guruprasad Rao <pranavrao2500@gmail.com>
2023-12-13 17:28:39 +0530

    Decrease no of feasible flights

--- Quantum Solutions/QAOA.py
+++ Quantum Solutions/QAOA.py
@@ -5,11 +5,13 @@ import datetime as dt
 from qiskit_algorithms import QAOA
 from qiskit_optimization import QuadraticProgram
 from qiskit_optimization.converters import QuadraticProgramToQubo
+from qiskit_algorithms import NumPyMinimumEigensolver
 from qiskit_optimization.algorithms import MinimumEigenOptimizer, WarmStartQAOAOptimizer, CplexOptimizer, CobylaOptimizer
 from qiskit.primitives import Sampler
 from qiskit_algorithms.optimizers import COBYLA
 from qiskit_algorithms.utils.algorithm_globals import algorithm_globals
-
+import copy
+import sys
 """
     We are creating a QUBO instance of a Quadratic Program and solving it using Warm Start QAOA.
      
@@ -36,9 +38,10 @@ class QuantumSolver:
     highval = 1000000  # used for G (neglecting some flights)
     inv_id: str  # stores the affected flight inventory id
     flight = None  # affected flight row
-    Q = A = B = N = G = None  # matrices to be used in Quadratic Program
+    FullList: dict
+    Q = A = B = Nb = Na = G = Delay = None  # matrices to be used in Quadratic Program
 
-    def __init__(self, inv_id, inFile):
+    def __init__(self, inv_id, FullList, inFile):
         """
             Input : Corresponding inventory id of affected flight from the provided datafile
             Creates a list of relevant flights, initializes matrices, and calls the QuantumSolve() function
@@ -48,24 +51,38 @@ class QuantumSolver:
 
         startTime = dt.datetime.now()
         self.inv_id = inv_id
+        self.FullList = FullList
+
         self.lst = self.__preProcess()  # list of relevant flights
         if len(self.lst) == 0:  # no relevant flights present
             raise Exception('No alternate flights')
-        print(len(self.lst), "Total Flights:\n")
-        # for i in self.lst:
-        #     print(i)
+        print(len(self.lst), "Feasible Flights\n")
+
         self.length = len(self.lst)
+        self.length = min(self.length, 10)
+        file = open("Selected Flights", "w")
+        for i in self.lst[:self.length]:
+            file.write(str(i) + "\n\n")
+        file.close()
 
         # all matrices
         self.Q = np.zeros((self.length, self.length))
-        self.A, self.B, self.N, self.G = np.zeros_like(self.Q), np.zeros_like(self.Q), np.zeros_like(self.Q), np.zeros_like(self.Q)
+        self.A, self.B, self.Nb, self.G, self.Na = np.zeros_like(self.Q), np.zeros_like(self.Q), np.zeros_like(
+            self.Q), np.zeros_like(self.Q), np.zeros_like(self.Q)
+        self.Delay = np.zeros((self.length,))
 
         print("\nAffected Flight\n")
         print(self.flight)
-        ans = self.quantumSolve()  # invoke the quantum solve function
-        print(ans)
 
-        print((dt.datetime.now() - startTime))
+        ans = self.quantumSolve()  # invoke the quantum solve function
+        TimeTaken = (dt.datetime.now() - startTime)
+
+        for i in range(len(ans)):
+            print("Solution", i, end="\n\n")
+            print(ans[i])
+            print("\n")
+
+        print(TimeTaken)
 
     def __diff(self, date1, time1, date2, time2):
         """
@@ -83,22 +100,49 @@ class QuantumSolver:
         index = self.df.loc[self.df["InventoryId"] == self.inv_id].index[0]  # get the index from the dataset
         self.flight = self.df.loc[index]  # affected flight
         list_of_feasible_flights = []
+        currFlTime = self.__diff(date1=self.flight["DepartureDate"], time1=self.flight["DepartureTime"],
+                                 date2=self.flight["ArrivalDate"],
+                                 time2=self.flight["ArrivalTime"])
 
         for i in range(len(self.df)):
             data = self.df.loc[i]
             if index == i:
                 continue
-            ti = self.__diff(date1=self.flight["DepartureDate"], time1=self.flight["DepartureTime"], date2=data["ArrivalDate"],
-                             time2=data["ArrivalTime"])
-            ti2 = self.__diff(date1=self.flight["DepartureDate"], time1=self.flight["DepartureTime"], date2=data["DepartureDate"],
-                              time2=data["DepartureTime"])
-            # print(data["InventoryId"], ti, ti2)
-            if ti < 60 or ti2 > 72 * 60:  # following the Minimum Ground Time and Maximum Connecting Time
+            fromlst = self.FullList.get(data["InventoryId"])
+            if fromlst:
+                if fromlst == "Cancelled":
+                    continue
+            if data["ArrivalAirport"] == self.flight["DepartureAirport"] or data["DepartureAirport"] == self.flight[
+                "ArrivalAirport"]:
+                continue
+            timebtwArr_Curdep = self.__diff(date1=self.flight["DepartureDate"], time1=self.flight["DepartureTime"],
+                                            date2=data["ArrivalDate"],
+                                            time2=data["ArrivalTime"])
+            timebtwdep_CurrDep = self.__diff(date1=self.flight["DepartureDate"], time1=self.flight["DepartureTime"],
+                                             date2=data["DepartureDate"],
+                                             time2=data["DepartureTime"])
+            flightTime = self.__diff(date1=data["DepartureDate"], time1=data["DepartureTime"],
+                                     date2=data["ArrivalDate"],
+                                     time2=data["ArrivalTime"])
+
+            if timebtwArr_Curdep < 60 or timebtwdep_CurrDep > 72 * 60:  # rule 1,2,3  # following the Minimum Ground Time and Maximum Connecting Time
+                continue
+            elif flightTime > currFlTime and data["DepartureAirport"] != self.flight["DepartureAirport"] \
+                    and data["ArrivalAirport"] != self.flight["ArrivalAirport"]:  # rule 4
                 continue
             else:
                 list_of_feasible_flights.append(data)
 
-        return list_of_feasible_flights
+        airports = [self.flight["DepartureAirport"]]
+        feas = []
+        while len(airports) != 0:
+            airport = airports.pop()
+            for i in list_of_feasible_flights:
+                if i["DepartureAirport"] == airport:
+                    feas += [i]
+                    airports += i["ArrivalAirport"]
+
+        return feas
 
     def __run(self):
         """
@@ -107,7 +151,7 @@ class QuantumSolver:
         self.graph = dict()  # to keep a tally of unique flights
 
         # Nodes
-        for i in range(len(self.lst)):
+        for i in range(self.length):
             data = self.lst[i]  # a single flight
 
             if data["InventoryId"] not in self.graph:
@@ -125,46 +169,51 @@ class QuantumSolver:
                                            data["ArrivalTime"]) * 100
 
                 # A Matrix measures if the current flight departs from the same airport as Affected flight
-                # N Matrix measures the neighbouring flight connectivity
+                # Nb Matrix measures the neighbouring flight connectivity(by deparuture)
+                # Na Matrix measures the neighbouring flight connectivity(by arrival)
                 # G Matrix measures the ground time spent by a flight (next departure - arrival)
                 if data["DepartureAirport"] == self.flight["DepartureAirport"]:
                     self.A[i, i] = 1
-                    self.N[i, i] = 1
-                    self.G[i, i] = self.__diff(self.flight["DepartureDate"], self.flight["DepartureTime"],
-                                               data["DepartureDate"], data["DepartureTime"]) * 100
+                    self.Nb[i, i] = 1
+                    # self.G[i, i] = self.__diff(self.flight["DepartureDate"], self.flight["DepartureTime"],
+                    #                            data["DepartureDate"], data["DepartureTime"]) * 100
                 else:
                     self.A[i, i] = 0
-                self.B[i, i] = 1 if (data["ArrivalAirport"] == self.flight["ArrivalAirport"]) else 0
+                if data["ArrivalAirport"] == self.flight["ArrivalAirport"]:
+                    self.Na[i, i] = 1
+                    self.B[i, i] = 1
+                    self.Delay[i] = int(
+                        self.__diff(self.flight["DepartureDate"], self.flight["DepartureTime"], data["ArrivalDate"],
+                                    data["ArrivalTime"]) * 100)
+                else:
+                    self.B[i, i] = 0
 
                 # Edges
-                for j in range(len(self.lst)):
+                for j in range(self.length):
                     fl2 = self.lst[j]
-                    if self.N[i, i] == 1 and i != j:
+                    if self.Nb[i, i] == 1 and i != j:
                         self.G[i, j] = 0
                     else:
-                        if data["InventoryId"] == fl2["InventoryId"]:
-                            ti = self.__diff(self.flight["DepartureDate"], self.flight["DepartureTime"],
-                                             fl2["DepartureDate"], fl2["DepartureTime"]) * 100
-                            self.G[i, j] = ti
-                        else:
-                            if data["DepartureAirport"] == fl2["ArrivalAirport"]:
-                                self.N[j, i] = 1
-                                ti = int(self.__diff(fl2["ArrivalDate"], fl2["ArrivalTime"], data["DepartureDate"],
-                                                     data[
-                                                         "DepartureTime"]) * 100)  # minutes(fl2["DepartureTime"]) - minutes(data["ArrivalTime"])
-                                if ti < 6000 or ti > 72000:
-                                    self.G[i, j] = 0
-                                else:
-                                    self.G[i, j] = ti
-                            else:
-                                self.N[j, i] = 0
+                        # if data["InventoryId"] == fl2["InventoryId"]:
+                        #     ti = self.__diff(self.flight["DepartureDate"], self.flight["DepartureTime"],
+                        #                      fl2["DepartureDate"], fl2["DepartureTime"]) * 100
+                        #     self.G[i, j] = ti
+                        # else:
+                        if data["DepartureAirport"] == fl2["ArrivalAirport"]:
+                            self.Nb[i, j] = 1
+                            ti = int(self.__diff(fl2["ArrivalDate"], fl2["ArrivalTime"], data["DepartureDate"],
+                                                 data[
+                                                     "DepartureTime"]) * 100)  # minutes(fl2["DepartureTime"]) - minutes(data["ArrivalTime"])
+                            if ti < 6000 or ti > 72000:
                                 self.G[i, j] = 0
+                            else:
+                                self.G[i, j] = ti
+                        else:
+                            self.G[i, j] = 0
+                    if data["ArrivalAirport"] == fl2["DepartureAirport"]:
+                        self.Na[i, j] = 1
         self.G = self.G.astype(int)
-        # print("Q:\n ", self.Q)
-        # print("G:\n ", self.G)
-        # print("A:\n ", self.A)
-        # print("B:\n ", self.B)
-        # print("N:\n ", self.N)
+
 
     def QAOA_algo(self) -> list:
         """
@@ -175,41 +224,54 @@ class QuantumSolver:
         Why QUBO - Each flight has two options - Either it can be in the path or not
         :return: list of alternate flights as
         """
-
         qp = QuadraticProgram("flights")
         F = self.Q + self.G  # quadratic form matrix
         F = F.astype(int)
+
         # print(F)
 
-        qp.minimize(quadratic=F)  # matrices fed into the quadratic program
+        qp.minimize(linear=self.Delay, quadratic=F)  # matrices fed into the quadratic program
         qp.binary_var_list(self.length)
         qp.linear_constraint(np.ones(self.length, ), ">", 1)
         qp.linear_constraint(self.A.diagonal(), "=", 1)
         qp.linear_constraint(self.B.diagonal(), "=", 1)
+        for i in range(self.length):
+            linear = copy.deepcopy(self.Nb[i, :])
+            linear[i] -= 1
+            qp.linear_constraint(linear, ">", 0)
+            print(linear)
+        # qp.quadratic_constraint(-1*np.ones((self.length,)),self.Nb,"=",0)
+        print(np.linalg.det(self.Na))
+        start1 = dt.datetime.now()
+        mod = qp.to_docplex()
+        mod.solve()
+        mod.print_solution()
+        print(dt.datetime.now() - start1)
         # print(qp.prettyprint())
+        # sys.exit(0)
 
         qp = QuadraticProgramToQubo().convert(qp)
-        # qp.quadratic_constraint(quadratic=self.N,linear=-1*np.ones((self.length,)),sense="=",rhs=0)
+        # qp.quadratic_constraint(quadratic=self.Nb,linear=-1*np.ones((self.length,)),sense="=",rhs=0)
         print(qp.prettyprint())
 
         # qubitOp, offset = qp.to_ising()  # conversion into Ising Problem
 
         two = TwoLocal(self.length, 'rx', 'cx', 'linear', reps=2, insert_barriers=True)  # an ansatz circuit
 
-        # start = dt.datetime.now()
+        start = dt.datetime.now()
         algorithm_globals.random_seed = 12345
 
         qaoa_mes = QAOA(sampler=Sampler(), optimizer=COBYLA())  # a qaoa instance of the quadratic program
         qaoa_mes.ansatz = two
         # ws_qaoa = WarmStartQAOAOptimizer(pre_solver=CobylaOptimizer(),relax_for_pre_solver=True,qaoa=qaoa_mes,epsilon=0.0)
-        ws_qaoa = WarmStartQAOAOptimizer(pre_solver=CplexOptimizer(), relax_for_pre_solver=True, qaoa=qaoa_mes,
+        ws_qaoa = WarmStartQAOAOptimizer(pre_solver=CplexOptimizer(), relax_for_pre_solver=False, qaoa=qaoa_mes,
                                          epsilon=0.0)
         # qaoa = MinimumEigenOptimizer(qaoa_mes)
         ws_qaoa_result = ws_qaoa.solve(qp)
         print(ws_qaoa_result.prettyprint())
 
         ans = list(ws_qaoa_result.variables_dict.values())
-        # print(dt.datetime.now() - start)
+        print(dt.datetime.now() - start)
 
         return ans
 
@@ -220,9 +282,14 @@ class QuantumSolver:
         """
         total = []
         self.__run()  # we will get the Q A B N G matrices initialised now
-        # for i in range(2):
-        ans = self.QAOA_algo()
-        total.append(self.__postProcess(ans[:self.length]))
+        prevAns = []
+        # while True:
+        for i in range(1):
+            ans = self.QAOA_algo()
+            # if ans == prevAns:
+            #     break
+            # prevAns=ans
+            total.append([self.__postProcess(ans[:self.length])])
         return total
 
     def __postProcess(self, bitString: list) -> list:
@@ -232,11 +299,22 @@ class QuantumSolver:
         :return: list of flights corresponding to the bitString
         """
         flights = []
+        isStarting = False;
+        isEnding = False  # TO check if path is complete (If not complete do no add this path)
         for i in range(len(bitString)):
             if bitString[i] == 1:
                 flights.append(self.lst[i])
-        # for i in range(len(flights)):
-        #     for j in range(len(flights)):
-        #         if i!=j :
-        #             self.Q[i,j]=self.highval
+        #         if self.lst[i]["DepartureAirport"]==self.flight["DepartureAirport"]:
+        #             isStarting=True
+
+        #         if self.lst[i]["ArrivalAirport"]==self.flight["ArrivalAirport"]:
+        #             isEnding=True
+        # if not isEnding or not isStarting:
+        #     return []
+        for i in range(len(bitString)):
+            if bitString[i] == 0:
+                continue
+            for j in range(len(bitString)):
+                if bitString[j] == 1:
+                    self.Q[i, j] = self.highval
         return flights
